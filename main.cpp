4dd60ac818195b73cc5ac1fce82e055109708540
YangLi <liy57580@163.com>
2024-03-14 19:35:48 +0800

    demo1done

--- main.cpp
+++ main.cpp
@@ -10,23 +10,27 @@
 
 #include "threadpool.h"
 
-void testFun(void* arg){
+void testFun(void *arg)
+{
     printf("i = %d\n", *(int *)arg);
-    //注意：std::cout非线程安全
-    //std::cout << "i = " << *(int *)arg << std::endl;
+    // 注意：std::cout非线程安全
+    // std::cout << "i = " << *(int *)arg << std::endl;
 }
 
-int main(){
-    nThreadPool *pool = (nThreadPool*)malloc(sizeof(nThreadPool));
+int main()
+{
+    nThreadPool *pool = (nThreadPool *)malloc(sizeof(nThreadPool));
     int ret = nThreadPoolCreate(pool, 1000);
-    if (ret != 1){
+    if (ret != 1)
+    {
         perror("线程池初始化失败");
         printf("ret = %d\n", ret);
         return -1;
     }
     printf("线程池初始化成功\n");
     int i = 0;
-    for (i = 0; i < 1000; ++i) {
+    for (i = 0; i < 1000; ++i)
+    {
         nThreadPush(pool, testFun, &i, sizeof(int));
     }
 
@@ -37,17 +41,19 @@ int main(){
 
 #include "threadpool.hpp"
 
-void testFun(void* arg){
+void testFun(void *arg)
+{
     printf("i = %d\n", *(int *)arg);
 }
 
-
-int main(){
+int main()
+{
     ThreadPool *pool = new ThreadPool(1000, 2000);
 
     printf("线程池初始化成功\n");
     int i = 0;
-    for (i = 0; i < 1000; ++i) {
+    for (i = 0; i < 1000; ++i)
+    {
         pool->pushJob(testFun, &i, sizeof(int));
     }
 }
